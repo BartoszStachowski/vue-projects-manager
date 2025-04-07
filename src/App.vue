63db45c1513ae50df0aad4523fb217f6e41e8b0f
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-04-07 15:39:28 +0200

    connect with supabase

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient.ts';
+console.log(supabase);
+</script>
 
 <template>
   <main>
---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+  import.meta.env.VITE_SUPERBASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY,
+);
