e7c525f4fa136e3496e6045dea52a0c86f60ba93
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-04-08 14:29:24 +0200

    types

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient';
+import { ref } from 'vue';
+import type { Tables } from '../../../database/types.ts';
+
+const projects = ref<Tables<'projects'>[] | null>(null);
+(async () => {
+  const { data, error } = await supabase.from('projects').select();
+
+  if (error) console.log(error);
+
+  projects.value = data;
+})();
+</script>
 
 <template>
   <div>
     <h1>Project page</h1>
     <RouterLink to="/">Go to home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
