9c142ee652631d48af2cb1045c9a910af9ab011f
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-04-08 16:40:50 +0200

    add shadcn-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { fileURLToPath, URL } from 'node:url';
 import VueRouter from 'unplugin-vue-router/vite';
 
+import tailwindcss from '@tailwindcss/vite';
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueRouter(), vueDevTools()],
+  plugins: [vue(), tailwindcss(), VueRouter(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
