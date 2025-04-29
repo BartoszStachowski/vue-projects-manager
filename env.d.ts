de6df8357a17eb02dcf66d919506068095697e68
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-04-29 08:43:55 +0200

    errors

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-vue-router/client" />
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<unknown, unknown, unknown>;
+  export default component;
+}
