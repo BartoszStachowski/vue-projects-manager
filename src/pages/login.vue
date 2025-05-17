50e47ae4edc79839d5221ebda8fcc2c2e20bf785
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-05-17 10:37:48 +0200

    Add useFormErrors composable; integrate error handling in login form

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -6,17 +6,16 @@ const formData = ref({
   password: '',
 });
 
-const _error = ref('');
+const { serverError, handleServerError } = useFormErrors();
 
 const router = useRouter();
 
 const signin = async () => {
   const { error } = await login(formData.value);
-  console.log('error', error?.message);
-  console.log(typeof error);
   if (!error) router.push('/');
-
-  _error.value = error?.message ?? '';
+  else {
+    handleServerError(error);
+  }
 };
 </script>
 
@@ -41,7 +40,7 @@ const signin = async () => {
               placeholder="johndoe19@example.com"
               required
               v-model="formData.email"
-              :class="{ 'border-red-500': _error }"
+              :class="{ 'border-red-500': serverError }"
             />
           </div>
           <div class="grid gap-2">
@@ -55,11 +54,11 @@ const signin = async () => {
               autocomplete
               required
               v-model="formData.password"
-              :class="{ 'border-red-500': _error }"
+              :class="{ 'border-red-500': serverError }"
             />
           </div>
-          <ul v-if="_error" class="text-sm text-left text-red-500">
-            <li class="list-disc">{{ _error }}</li>
+          <ul v-if="serverError" class="text-sm text-left text-red-500">
+            <li class="list-disc">{{ serverError }}</li>
           </ul>
           <Button type="submit" class="w-full">Login</Button>
         </form>
