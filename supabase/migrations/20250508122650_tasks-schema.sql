c9f889ce26b191ac2bf2c24e63458863e9891c89
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-05-17 09:44:07 +0200

    Enhance database seeding; add primary test user creation and update tasks schema

--- supabase/migrations/20250508122650_tasks-schema.sql
+++ supabase/migrations/20250508122650_tasks-schema.sql
@@ -1,15 +1,14 @@
-drop table if exists projects;
-
-drop type if exists current_status;
-create type current_status as enum ('in-progress', 'completed');
+drop table if exists tasks;
 
 create table 
-  projects (
+  tasks (
     id bigint primary key generated always as identity not null,
     created_at timestamptz default now() not null,
     name text not null,
-    slug text unique not null,
-    description text,
     status current_status default 'in-progress' not null,
+    description text not null,
+    due_date date default null,
+    profile_id uuid references profiles (id) on delete cascade not null,
+    project_id bigint references projects (id) default null,
     collaborators text array default array[]::varchar[] not null
   );
