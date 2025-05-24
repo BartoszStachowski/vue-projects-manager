10bc1c7c40eeaa1fc1734e375097b1675c0a9453
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-05-24 19:31:39 +0200

    Refactor project store; improve projects state management and cache validation

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -2,15 +2,34 @@ import { projectsQuery, type Projects } from '@/utils/supaQueries';
 import { useMemoize } from '@vueuse/core';
 
 export const useProjectStore = defineStore('project-store', () => {
-  const projects = ref<Projects | null>(null);
+  const projects = ref<Projects>([]);
+
+  // Memoize the projectsQuery to avoid unnecessary re-fetching
   const loadProjects = useMemoize(async (key: string) => await projectsQuery);
+
+  const validateCache = () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data, error }) => {
+        // Check if the data has changed
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          return;
+        } else {
+          // Clear the cache if the data has changed
+          loadProjects.delete('projects');
+          if (!error && data) projects.value = data;
+        }
+      });
+    }
+  };
 
   const getProjects = async () => {
     const { data, error, status } = await loadProjects('projects');
 
     if (error) useErrorStore().setError({ error, customCode: status });
 
-    projects.value = data;
+    if (data) projects.value = data;
+
+    validateCache();
   };
 
   return {
