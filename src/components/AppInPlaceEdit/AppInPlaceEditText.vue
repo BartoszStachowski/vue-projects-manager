d3577f358dab4cb3968287a315ddd0ee06011c84
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-06-01 10:43:35 +0200

    Refactor AppInPlaceEditText component to enhance input value handling and emit logic

--- src/components/AppInPlaceEdit/AppInPlaceEditText.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditText.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
-const value = defineModel();
+const inputValue = defineModel<string>({ required: true });
 
-defineEmits(['commit']);
+const emit = defineEmits(['commit']);
+
+const oldInputValue = ref(inputValue.value);
+watch(inputValue, (newValue, oldValue) => {
+  oldInputValue.value = oldValue;
+});
+
+const emitCommit = () => {
+  if (inputValue.value !== oldInputValue.value) {
+    emit('commit');
+  }
+};
 </script>
 <template>
   <input
     class="w-full focus:outline-hidden p-1 bg-transparent focus:border-none focus:bg-gray-800 focus:rounded-md"
     type="text"
-    v-model="value"
-    @blur="$emit('commit')"
+    v-model="inputValue"
+    @blur="emitCommit"
     @keyup.enter="($event.target as HTMLInputElement).blur()"
   />
 </template>
